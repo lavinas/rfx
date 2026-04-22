c4346ce8da328311a8ce47a6edc7d6b3a8dadeb6
lavinas <lavinas@me.com>
2026-04-21 22:19:27 -0300

    inserts scripts

---
+++ reg/go/consolidator/scripts/insert_contatos.sql
@@ -0,0 +1,11 @@
+insert into cadoc_6334_v2.contatos (year, created_at, updated_at, quarter, contact_type, name, position, phone_number, email)
+values (2026, now(), now(), 1, 'D', 'Diego Silveira de Faria', 'Diretor', '(65) 99683-4775', 'diego@redeflex.com.br');
+
+insert into cadoc_6334_v2.contatos (year, created_at, updated_at, quarter, contact_type, name, position, phone_number, email)
+values (2026, now(), now(), 1, 'T', 'Werika Oliveira Calassa', 'Gerente de Controladoria', '(65) 99964-2769', 'werika.calassa@redeflex.com.br');
+
+insert into cadoc_6334_v2.contatos (year, created_at, updated_at, quarter, contact_type, name, position, phone_number, email)
+values (2026, now(), now(), 1, 'T', 'Andrea Da Silva Santos', 'Coordenador Fiscal', '(65) 99901-7193', 'andrea.dasantos@redeflex.com.br');
+
+insert into cadoc_6334_v2.contatos (year, created_at, updated_at, quarter, contact_type, name, position, phone_number, email)
+values (2026, now(), now(), 1, 'I', '', '', '', 'atendimento.bacen@redeflex.com.br');
---
+++ reg/go/consolidator/scripts/insert_luccred.sql
@@ -0,0 +1,22 @@
+create table cadoc_6334_v2.luccred_save as
+select * from cadoc_6334_v2.luccred;
+
+
+update luccred
+   set gross_revenue =      7884916.66,
+       rental_revenue =     2183751.22,
+       others_revenue =     1512410.24,
+       interchange_cost =   3910021.60,
+       marketing_cost =      191077.94,
+       brand_access_cost =  1515123.96,
+       risk_cost =                0.00,
+       processing_cost =     406930.64,
+       others_cost =        5686873.32
+ where year = 2026
+   and quarter = 1;
+
+
+select gross_revenue + rental_revenue + others_revenue - interchange_cost - marketing_cost - brand_access_cost - risk_cost - processing_cost - others_cost as cost
+  from cadoc_6334_v2.luccred
+ where year = 2026
+   and quarter = 1;
